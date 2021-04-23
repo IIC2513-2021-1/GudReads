c9fb31975d3d813587534bd2fc750d1699776129
Benjamín Domínguez <bidominguez@uc.cl>
2021-04-23 16:57:47 -0400

    feat: Add authors show

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -2,12 +2,24 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.param('id', async (id, ctx, next) => {
+  ctx.state.author = await ctx.orm.author.findByPk(ctx.params.id);
+  if (!ctx.state.author) return ctx.throw(404);
+  return next();
+});
+
 router.get('authors.list', '/', async (ctx) => {
   const authorsList = await ctx.orm.author.findAll();
   await ctx.render('authors/index', {
     authorsList,
+    authorPath: (author) => ctx.router.url('authors.show', { id: author.id }),
     notice: ctx.flashMessage.notice,
   });
+});
+
+router.get('authors.show', '/:id', async (ctx) => {
+  const { author } = ctx.state;
+  await ctx.render('authors/show', { author, notice: ctx.flashMessage.notice });
 });
 
 module.exports = router;
