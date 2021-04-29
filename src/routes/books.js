8d97eecbe77605ad3f7d844514837f5bb94da88a
meretamal <meretamal@uc.cl>
2021-04-29 14:51:55 -0400

    feat: add books routes

---
+++ src/routes/books.js
@@ -0,0 +1,31 @@
+const KoaRouter = require('koa-router');
+
+const router = new KoaRouter();
+
+router.get('books.new', '/new', async (ctx) => {
+  const book = ctx.orm.book.build();
+  const authorList = await ctx.orm.author.findAll();
+  await ctx.render('books/new', {
+    book,
+    authorList,
+    submitBookPath: ctx.router.url('books.create'),
+  });
+});
+
+router.post('books.create', '/', async (ctx) => {
+  const book = ctx.orm.book.build(ctx.request.body);
+  try {
+    await book.save({ field: ['title', 'publication', 'authorId'] });
+    ctx.redirect(ctx.router.url('authors.show', { id: book.authorId }));
+  } catch (ValidationError) {
+    const authorList = await ctx.orm.author.findAll();
+    await ctx.render('books/new', {
+      book,
+      authorList,
+      errors: ValidationError.errors,
+      submitBookPath: ctx.router.url('books.create'),
+    });
+  }
+});
+
+module.exports = router;
