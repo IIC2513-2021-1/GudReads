d30e02e87d15363c02d47ee3d37446465ffb2b16
meretamal <meretamal@uc.cl>
2021-04-29 14:52:42 -0400

    feat: add books routes to main router

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,15 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 const authors = require('./routes/authors');
+const books = require('./routes/books');
+const booksForAuthors = require('./routes/booksForAuthors');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/authors', authors.routes());
+router.use('/books', books.routes());
+router.use('/authors/:authorId/books', booksForAuthors.routes());
 
 module.exports = router;
