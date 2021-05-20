12c108a8f653f18752d36417b3c3d1e0ff2e2c25
Sebastián Vicencio <sivicencio@gmail.com>
2021-05-19 21:55:22 -0400

    test(auth): add test for setCurrentUser middleware

--- src/middlewares/auth.test.js
+++ src/middlewares/auth.test.js
@@ -1,9 +1,7 @@
 const orm = require('../models');
-const { checkAuth } = require('./auth');
+const { checkAuth, setCurrentUser } = require('./auth');
 
-describe('checkAuth middleware', () => {
-  const ctx = {};
-
+describe('Auth middlewares', () => {
   beforeAll(async () => {
     await orm.sequelize.sync({ force: true });
   });
@@ -12,34 +10,99 @@ describe('checkAuth middleware', () => {
     await orm.sequelize.close();
   });
 
-  describe('when currentUser exists inside ctx.state', () => {
-    beforeAll(async () => {
-      const userData = {
-        firstName: 'John',
-        lastName: 'Web',
-        email: 'user@example.org',
-        password: '12345',
-      };
-      ctx.state = {
-        currentUser: await orm.user.create(userData),
-      };
+  describe('checkAuth middleware', () => {
+    const ctx = {};
+
+    describe('when currentUser exists inside ctx.state', () => {
+      beforeAll(async () => {
+        const userData = {
+          firstName: 'John',
+          lastName: 'Web',
+          email: 'user@example.org',
+          password: '12345',
+        };
+        ctx.state = {
+          currentUser: await orm.user.create(userData),
+        };
+      });
+
+      test('returns undefined (it means everything is ok)', async () => {
+        const result = await checkAuth(ctx, () => {});
+        expect(result).toBeUndefined();
+      });
     });
 
-    test('returns undefined (it means everything is ok)', async () => {
-      const result = await checkAuth(ctx, () => {});
-      expect(result).toBeUndefined();
+    describe('when currentUser does not exist inside ctx.state', () => {
+      beforeAll(() => {
+        ctx.state = {};
+        ctx.throw = jest.fn(); // Mock ctx.throw fn
+      });
+
+      test('calls ctx.throw with 401 status code (unauthorized)', async () => {
+        await checkAuth(ctx, () => {});
+        expect(ctx.throw).toHaveBeenCalledWith(401);
+      });
     });
   });
 
-  describe('when currentUser does not exist inside ctx.state', () => {
-    beforeAll(() => {
-      ctx.state = {};
-      ctx.throw = jest.fn(); // Mock ctx.throw fn
+  describe('setCurrentUser middleware', () => {
+    const ctx = { orm };
+
+    describe('when currentUserId exists inside ctx.session', () => {
+      let user;
+
+      beforeAll(async () => {
+        const userData = {
+          firstName: 'Jane',
+          lastName: 'Doe',
+          email: 'jane@example.org',
+          password: '12345',
+        };
+        user = await orm.user.create(userData);
+      });
+
+      describe('when there is a user with id equals to ctx.session.currentUserId', () => {
+        beforeAll(async () => {
+          ctx.session = {
+            currentUserId: user.id,
+          };
+          ctx.state = {};
+
+          await setCurrentUser(ctx, () => {});
+        });
+
+        test('sets ctx.state.currentUser with the instance of the user', async () => {
+          expect(ctx.state.currentUser.toJSON()).toEqual(user.toJSON());
+        });
+      });
+
+      describe('when there is no user with id equals to ctx.session.currentUserId', () => {
+        beforeAll(async () => {
+          ctx.session = {
+            currentUserId: user.id + 10, // Use a different authorId
+          };
+          ctx.state = {};
+
+          await setCurrentUser(ctx, () => {});
+        });
+
+        test('does not set ctx.state.currentUser', async () => {
+          expect(ctx.state.currentUser).toBeNull();
+        });
+      });
     });
 
-    test('calls ctx.throw with 401 status code (unauthorized)', async () => {
-      await checkAuth(ctx, () => {});
-      expect(ctx.throw).toHaveBeenCalledWith(401);
+    describe('when currentUserId does not exist inside ctx.session', () => {
+      beforeAll(async () => {
+        ctx.session = {};
+        ctx.state = {};
+
+        await setCurrentUser(ctx, () => {});
+      });
+
+      test('does not set ctx.state.currentUser', async () => {
+        expect(ctx.state.currentUser).toBeUndefined();
+      });
     });
   });
 });
