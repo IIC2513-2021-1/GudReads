60c975620ded0659a942d76b75306398f0f5433b
Diego Echeverría <dhecheverria@uc.cl>
2021-06-18 19:23:19 -0400

    feat: add new route for authors api to update authors

--- src/routes/api/authors.js
+++ src/routes/api/authors.js
@@ -2,7 +2,7 @@ const KoaRouter = require('koa-router');
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 const AuthorSerializer = new JSONAPISerializer('authors', {
-  attributes: ['firstName', 'lastName', 'birthDate'],
+  attributes: ['firstName', 'lastName', 'birthDate', 'imageUrl'],
   keyForAttribute: 'camelCase',
 });
 
@@ -21,6 +21,23 @@ router.post('api.authors.create', '/', async (ctx) => {
     const author = ctx.orm.author.build(ctx.request.body);
     await author.save({ fields: ['firstName', 'lastName', 'birthDate'] });
     ctx.status = 201;
+    ctx.body = AuthorSerializer.serialize(author);
+  } catch (ValidationError) {
+    ctx.throw(400, 'Bad request');
+  }
+});
+
+router.patch('api.authors.update', '/:id', async (ctx) => {
+  const { cloudinary } = ctx.state;
+  try {
+    // Remover línea siguiente si se agrega un router.params
+    const author = await ctx.orm.author.findByPk(ctx.params.id);
+    const { image } = ctx.request.files;
+    if (image.size > 0) {
+      const imageUrl = await cloudinary.uploader.upload(image.path);
+      ctx.request.body.imageUrl = imageUrl.url;
+    }
+    await author.update(ctx.request.body, { fields: ['firstName', 'lastName', 'birthDate', 'imageUrl'] });
     ctx.body = AuthorSerializer.serialize(author);
   } catch (ValidationError) {
     ctx.throw(400, 'Bad request');
